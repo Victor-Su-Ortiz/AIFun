ea6b796ea713f13177a0f32584c3fcff7c06c76d
Victor Su-Ortiz <vas64@cornell.edu>
2024-02-17 01:07:13 -0500

    chat

---
+++ aigamechat/src/App.js
@@ -0,0 +1,112 @@
+import React, { useState, useRef } from "react";
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  collection,
+  query,
+  orderBy,
+  limit,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+const app = initializeApp({
+  apiKey: "AIzaSyC6Icwsl9HSb-Kp2c5u54ECN8pyogBaEVE",
+  authDomain: "aigamechat.firebaseapp.com",
+  projectId: "aigamechat",
+  storageBucket: "aigamechat.appspot.com",
+  messagingSenderId: "490010703083",
+  appId: "1:490010703083:web:6ce2a71c04a40215097aae",
+  measurementId: "G-J18GDPTRY1",
+});
+
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+function App() {
+  const [user] = useAuthState(auth);
+  return (
+    <div className="App">
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
+      </header>
+      {/* <header className="App-header"></header> */}
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+  return <button onClick={signInWithGoogle}>Sign in with google</button>;
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+  );
+}
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = collection(firestore, "messages");
+  const messageQuery = query(messagesRef, orderBy("createdAt"), limit(25));
+
+  const [messages] = useCollectionData(messageQuery, { idField: "id" });
+  const [formValue, setFormValue] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const { uid, photoURL } = auth.currentUser;
+    // Add a new document with a generated ID
+    await addDoc(messagesRef, {
+      text: formValue,
+      uid,
+      photoURL,
+      createdAt: serverTimestamp(), // Corrected serverTimestamp usage
+    });
+
+    setFormValue(""); // Reset form value
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+
+        <span ref={dummy}></span>
+      </main>
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        />
+        <button type="submit">submit</button>
+      </form>
+    </>
+  );
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} />
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default App;
